d10f4ac4204f622b004976e3118f45d96e7ce920
lisa-go <lis-zaa@live.com>
2022-11-10 05:30:40 -0600

    some display additions

--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ import thermostatPic from './images/thermostat.svg';
 import humidityPic from './images/humidity.svg';
 import cloudPic from './images/cloud.svg';
 import fogPic from './images/foggy.svg';
-import moonPic from './images/moon.svg';
-import pclouddayPic from './images/partly_cloudy_day.svg';
-import pcloudnightPic from './images/partly_cloudy_night.svg';
+import clearPic from './images/clearsky.svg'
 import rainPic from './images/rainy.svg';
 import snowPic from './images/snowing.svg';
-import sunPic from './images/sunny.svg';
 import thunderPic from './images/thunderstorm.svg';
-import clearnightBg from './images/clear_night-adrian-unsplash.jpg';
-import clearskyBg from './images/clear_sky-anders-jilden-unsplash.jpg';
+import clearskyBg from './images/clear-cristofer-unsplash.jpg';
 import mistBg from './images/mist-clyde-he-unsplash.jpg';
 import rainBg from './images/rain-noah-silliman-unsplash.jpg';
-import snowBg from './images/snow-fabian-mardi-unsplash.jpg';
+import snowBg from './images/snow-aditya-vyas-unsplash.jpg';
 import thunderBg from './images/thunder-levi-guzman-unsplash.jpg';
-import cloudyBg from './images/cloudy-billy-huynh-unsplash.jpg';
+import cloudyBg from './images/cloudy-tengyart-unsplash.jpg';
 const location = document.querySelector('#location');
 
 async function getWeather(choice) {
@@ -80,7 +76,7 @@ function getForecast() {
                 elements[0].parentNode.removeChild(elements[0]);
             }
 
-        for (let i = 0; i < weatherData.list.length; i = i + 8) {
+        for (let i = 0, j = 0; i < weatherData.list.length; i = i + 8, j++) {
             const fcard = document.createElement('fcard');
             fcont.appendChild(fcard);
 
@@ -107,43 +103,43 @@ function getForecast() {
                 fimg.style.background = 'url(' + img + ')';
             }
 
-            if (wid == 800 && timeConverter(weatherData.dt).hour > 7 && timeConverter(weatherData.dt).hour < 17) {
-                const img = sunPic;
-                fimg.style.background = 'url(' + img + ')';
-            }
-            else if (wid == 800) {
-                const img = moonPic;
+            if (wid == 800) {
+                const img = clearPic;
                 fimg.style.background = 'url(' + img + ')';
             }
 
-            if (wid == 801 && timeConverter(weatherData.dt).hour > 7 && timeConverter(weatherData.dt).hour < 17) {
-                const img = pclouddayPic;
-                fimg.style.background = 'url(' + img + ')';
-            }
-            else if (wid == 801) {
-                const img = pcloudnightPic;
-                fimg.style.background = 'url(' + img + ')';
-            }
-
-            if (wid >= 802 && wid <= 804) {
+            if (wid >= 801 && wid <= 804) {
                 const img = cloudPic;
                 fimg.style.background = 'url(' + img + ')';
             }
             fimg.style.backgroundRepeat = 'no-repeat';
             fimg.style.backgroundSize = 'contain';
-            fcard.appendChild(fimg)
+            fcard.appendChild(fimg);
 
             const fdate = document.createElement('fdate');
-            fdate.textContent = timeConverter(weatherData.list[i].dt);
+            fdate.textContent = daysFromToday(j) + ' ' + timeConverter(weatherData.list[i].dt);
             fcard.appendChild(fdate);
 
-            const ftemp = document.createElement('ftemp');
-            ftemp.textContent = weatherData.list[i].main.temp + ' °F';
-            fcard.appendChild(ftemp);
+            const fwhem = document.createElement('fwhem');
+            fwhem.textContent = weatherData.list[i].weather[0].main;
+            fcard.appendChild(fwhem);
 
-            const ftempmm = document.createElement('ftempmm');
-            ftempmm.textContent = weatherData.list[i].main.temp_min + ' °F' + ' — ' + weatherData.list[i].main.temp_max + ' °F';
-            fcard.appendChild(ftempmm);
+            const fwhed = document.createElement('fwhed');
+            fwhed.textContent = weatherData.list[i].weather[0].description;
+            fcard.appendChild(fwhed);
+
+            const ftemp = document.createElement('ftemp');
+            fcard.appendChild(ftemp);
+                const tempimg = new Image();
+                tempimg.src = thermostatPic;
+                ftemp.appendChild(tempimg);
+                const ftempn = document.createElement('ftempn');
+                ftempn.textContent = weatherData.list[i].main.temp + ' °F';
+                ftemp.appendChild(ftempn);
+
+            const ftempf = document.createElement('ftempm');
+            ftempf.textContent = 'Feels like ' + weatherData.list[i].main.feels_like + ' °F';
+            fcard.appendChild(ftempf);
         }
         
     });
@@ -156,9 +152,8 @@ function showData(){
         document.querySelector('.w-infod').textContent = weatherData.weather[0].description;
         document.querySelector('.w-location').textContent = weatherData.name + ', ' + weatherData.sys.country;       
         document.querySelector('.wtemp').textContent = weatherData.main.temp + ' °F';
-        document.querySelector('.w-tempfeel').textContent = weatherData.main.feels_like + ' °F';
-        document.querySelector('.w-tempmm').textContent = weatherData.main.temp_min  + ' °F' + ' — ' + weatherData.main.temp_max + ' °F';
-        document.querySelector('.w-datetime').textContent = timeConverter(weatherData.dt);
+        document.querySelector('.w-tempfeel').textContent = 'Feels like ' + weatherData.main.feels_like + ' °F';
+        document.querySelector('.w-datetime').textContent = daysFromToday(0) + ' ' + timeConverter(weatherData.dt);
         document.querySelector('.whumid').textContent = 'Humidity: ' + weatherData.main.humidity + '%';
             
   
@@ -192,33 +187,14 @@ function showData(){
                 document.body.style.background = 'url(' + bg + ')';
             }
 
-            if (wid == 800 && timeConverter(weatherData.dt).hour > 7 && timeConverter(weatherData.dt).hour < 17) {
-                const img = sunPic;
+            if (wid == 800) {
+                const img = clearPic;
                 wimgc.style.background = 'url(' + img + ')';
                 const bg = clearskyBg;
                 document.body.style.background = 'url(' + bg + ')';
             }
-            else if (wid == 800) {
-                const img = moonPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = clearnightBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
 
-            if (wid == 801 && timeConverter(weatherData.dt).hour > 7 && timeConverter(weatherData.dt).hour < 17) {
-                const img = pclouddayPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = clearskyBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
-            else if (wid == 801) {
-                const img = pcloudnightPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = clearnightBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
-
-            if (wid >= 802 && wid <= 804) {
+            if (wid >= 801 && wid <= 804) {
                 const img = cloudPic;
                 wimgc.style.background = 'url(' + img + ')';
                 const bg = cloudyBg;
@@ -227,11 +203,22 @@ function showData(){
             wimgc.style.backgroundRepeat = 'no-repeat';
             wimgc.style.backgroundSize = '100%';
             document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundAttachment = 'fixed';
 
             getForecast();
     });
 
 };
+
+
+
+const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+function daysFromToday(num) {
+    const d = new Date();
+    const current = d.getDay();
+    let day = weekday[(current + num) % 7];
+    return day;
+}
 
 function timeConverter(UNIX_timestamp){
     var a = new Date(UNIX_timestamp * 1000);
@@ -240,8 +227,8 @@ function timeConverter(UNIX_timestamp){
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
-    var min = a.getMinutes();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+    var min = String(a.getMinutes()).padStart(2, '0');
+    var time = date + ' ' + month;
     return time;
   }
 
