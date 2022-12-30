7a02dc8ecd6c812c781d4ec38a531c0f6243da66
Lisa <lis-zaa@live.com>
2022-12-30 01:14:42 -0600

    format code

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ import gitIcon from './images/github.svg';
 const location = document.querySelector('#location');
 const degSwitch = document.querySelector('#degSwitch');
 
-
 async function getWeather(choice) {
     //* mode = weather or forecast *//
     var deg;
@@ -29,76 +28,74 @@ async function getWeather(choice) {
         else {
             deg = 'imperial';
         }
-    let mode = choice;
-    
-    const response = 
-        await fetch('https://api.openweathermap.org/data/2.5/' +
-        mode +
-        '?q=' +
-        location.value + 
-        '&APPID=24c9ca958f3c17129e987bac3597de6a' +
-        '&units=' +
-        deg );
+        let mode = choice;
 
-    const weatherData = await response.json();
-    console.log(weatherData);
-    return weatherData;
+        const response =
+            await fetch('https://api.openweathermap.org/data/2.5/' +
+                mode +
+                '?q=' +
+                location.value +
+                '&APPID=24c9ca958f3c17129e987bac3597de6a' +
+                '&units=' +
+                deg);
+
+        const weatherData = await response.json();
+        console.log(weatherData);
+        return weatherData;
     }
     catch (error) {
         console.log(error);
     }
 }
 
-
 //* default location *//
 if (location.value == '') {
     location.value = 'houston'
-    getWeather( 'weather')
-    .then((weatherData) => {
-        try {
-            showData();
-    }
-        catch {
-            console.log (weatherData);
-        }
-    });
+    getWeather('weather')
+        .then((weatherData) => {
+            try {
+                showData();
+            }
+            catch {
+                console.log(weatherData);
+            }
+        });
 }
 
-
 //* button event listener *//
-document.querySelector('#submit').addEventListener('click', function() {
+document.querySelector('#submit').addEventListener('click', function () {
     getWeather('weather')
-    .then((weatherData) => {
-        try {
-            showData();    
-    }
-        catch {
-            console.log (weatherData);
-        }
-    });
+        .then((weatherData) => {
+            try {
+                showData();
+            }
+            catch {
+                console.log(weatherData);
+            }
+        });
 });
 
 //* switch event listener *//
-document.querySelector('#degSwitch').addEventListener('click', function() {
+document.querySelector('#degSwitch').addEventListener('click', function () {
     getWeather('weather')
-    .then((weatherData) => {
-        try {
-            showData();    
-    }
-        catch {
-            console.log (weatherData);
-        }
-    });
+        .then((weatherData) => {
+            try {
+                showData();
+            }
+            catch {
+                console.log(weatherData);
+            }
+        });
 });
 
 function getForecast() {
     getWeather('forecast').then((weatherData) => {
         const fcont = document.querySelector('#forecastContainer');
-        
-            let elements = document.getElementsByTagName('fcard');
-            while(elements.length > 0){
-                elements[0].parentNode.removeChild(elements[0]);
-            }
+
+        let elements = document.getElementsByTagName('fcard');
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
 
         for (let i = 0, j = 0; i < weatherData.list.length; i = i + 8, j++) {
             const fcard = document.createElement('fcard');
@@ -107,7 +104,7 @@ function getForecast() {
 
             const fimg = document.createElement('fimg');
             let wid = weatherData.list[i].weather[0].id;
-            
+
             if (wid >= 200 && wid <= 232) {
                 const img = thunderPic;
                 fimg.style.background = 'url(' + img + ')';
@@ -117,7 +114,7 @@ function getForecast() {
                 const img = rainPic;
                 fimg.style.background = 'url(' + img + ')';
             }
-            
+
             if (wid >= 600 && wid <= 622) {
                 const img = snowPic;
                 fimg.style.background = 'url(' + img + ')';
@@ -142,16 +139,16 @@ function getForecast() {
             fcard.appendChild(fimg);
 
             const fday = document.createElement('fday');
-                if (j == 0) {
-                    fday.textContent = 'Today';
-                }
-                else if (j == 1) {
-                    fday.textContent = 'Tomorrow';
-                }
-                else {
-                    fday.textContent = daysFromToday(j);
-                }
-                fcard.appendChild(fday);
+            if (j == 0) {
+                fday.textContent = 'Today';
+            }
+            else if (j == 1) {
+                fday.textContent = 'Tomorrow';
+            }
+            else {
+                fday.textContent = daysFromToday(j);
+            }
+            fcard.appendChild(fday);
 
             const fdate = document.createElement('fdate');
             fdate.textContent = timeConverter(weatherData.list[i].dt);
@@ -175,12 +172,12 @@ function getForecast() {
 
             const ftemp = document.createElement('ftemp');
             fcard.appendChild(ftemp);
-                const tempimg = new Image();
-                tempimg.src = thermostatPic;
-                ftemp.appendChild(tempimg);
-                const ftempn = document.createElement('ftempn');
-                ftempn.textContent = Math.round(weatherData.list[i].main.temp * 10) / 10 + deg;
-                ftemp.appendChild(ftempn);
+            const tempimg = new Image();
+            tempimg.src = thermostatPic;
+            ftemp.appendChild(tempimg);
+            const ftempn = document.createElement('ftempn');
+            ftempn.textContent = Math.round(weatherData.list[i].main.temp * 10) / 10 + deg;
+            ftemp.appendChild(ftempn);
 
             const ftempf = document.createElement('ftempm');
             ftempf.textContent = 'Feels like ' + Math.round(weatherData.list[i].main.feels_like * 10) / 10 + deg;
@@ -195,11 +192,10 @@ function getForecast() {
             fhumidt.textContent = 'Humidity: ' + weatherData.list[i].main.humidity + '%';
             fhumid.appendChild(fhumidt);
         }
-        
     });
 }
 
-function showData(){
+function showData() {
     getWeather('weather').then((weatherData) => {
         var deg;
         if (degSwitch.checked) {
@@ -211,67 +207,64 @@ function showData(){
 
         document.querySelector('.w-info').textContent = weatherData.weather[0].main;
         document.querySelector('.w-infod').textContent = weatherData.weather[0].description;
-        document.querySelector('.w-location').textContent = weatherData.name + ', ' + weatherData.sys.country;       
+        document.querySelector('.w-location').textContent = weatherData.name + ', ' + weatherData.sys.country;
         document.querySelector('.wtemp').textContent = Math.round(weatherData.main.temp * 10) / 10 + deg;
         document.querySelector('.w-tempfeel').textContent = 'Feels like ' + Math.round(weatherData.main.feels_like * 10) / 10 + deg;
         document.querySelector('.w-datetime').textContent = daysFromToday(0) + ' ' + timeConverter(weatherData.dt);
         document.querySelector('.whumid').textContent = 'Humidity: ' + weatherData.main.humidity + '%';
-            
-  
+
         const wimgc = document.querySelector('.w-img');
         let wid = weatherData.weather[0].id;
-            if (wid >= 200 && wid <= 232) {
-                const img = thunderPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = thunderBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
+        if (wid >= 200 && wid <= 232) {
+            const img = thunderPic;
+            wimgc.style.background = 'url(' + img + ')';
+            const bg = thunderBg;
+            document.body.style.background = 'url(' + bg + ')';
+        }
 
-            if (wid >= 300 && wid <= 531) {
-                const img = rainPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = rainBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
-            
-            if (wid >= 600 && wid <= 622) {
-                const img = snowPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = snowBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
+        if (wid >= 300 && wid <= 531) {
+            const img = rainPic;
+            wimgc.style.background = 'url(' + img + ')';
+            const bg = rainBg;
+            document.body.style.background = 'url(' + bg + ')';
+        }
 
-            if (wid >= 701 && wid <= 781) {
-                const img = fogPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = mistBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
+        if (wid >= 600 && wid <= 622) {
+            const img = snowPic;
+            wimgc.style.background = 'url(' + img + ')';
+            const bg = snowBg;
+            document.body.style.background = 'url(' + bg + ')';
+        }
 
-            if (wid == 800) {
-                const img = clearPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = clearskyBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
+        if (wid >= 701 && wid <= 781) {
+            const img = fogPic;
+            wimgc.style.background = 'url(' + img + ')';
+            const bg = mistBg;
+            document.body.style.background = 'url(' + bg + ')';
+        }
 
-            if (wid >= 801 && wid <= 804) {
-                const img = cloudPic;
-                wimgc.style.background = 'url(' + img + ')';
-                const bg = cloudyBg;
-                document.body.style.background = 'url(' + bg + ')';
-            }
-            wimgc.style.backgroundRepeat = 'no-repeat';
-            wimgc.style.backgroundSize = '100%';
-            document.body.style.backgroundSize = 'cover';
-            document.body.style.backgroundAttachment = 'fixed';
+        if (wid == 800) {
+            const img = clearPic;
+            wimgc.style.background = 'url(' + img + ')';
+            const bg = clearskyBg;
+            document.body.style.background = 'url(' + bg + ')';
+        }
 
-            getForecast();
+        if (wid >= 801 && wid <= 804) {
+            const img = cloudPic;
+            wimgc.style.background = 'url(' + img + ')';
+            const bg = cloudyBg;
+            document.body.style.background = 'url(' + bg + ')';
+        }
+        wimgc.style.backgroundRepeat = 'no-repeat';
+        wimgc.style.backgroundSize = '100%';
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundAttachment = 'fixed';
+
+        getForecast();
     });
 
 };
-
-
 
 const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 function daysFromToday(num) {
@@ -281,9 +274,9 @@ function daysFromToday(num) {
     return day;
 }
 
-function timeConverter(UNIX_timestamp){
+function timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
@@ -291,20 +284,20 @@ function timeConverter(UNIX_timestamp){
     var min = String(a.getMinutes()).padStart(2, '0');
     var time = date + ' ' + month;
     return time;
-  }
+}
 
-  const thermIcon = new Image();
-  thermIcon.src = thermostatPic;
-  document.querySelector('.w-tempnow').appendChild(thermIcon);
+const thermIcon = new Image();
+thermIcon.src = thermostatPic;
+document.querySelector('.w-tempnow').appendChild(thermIcon);
 
-  const humIcon = new Image();
-  humIcon.src = humidityPic;
-  document.querySelector('.w-humidity').appendChild(humIcon);
+const humIcon = new Image();
+humIcon.src = humidityPic;
+document.querySelector('.w-humidity').appendChild(humIcon);
 
-  const githuba = document.createElement('a');
-  githuba.href = 'https://github.com/lisa-go';
-  githuba.classList.add('link');
-  document.querySelector('.header').appendChild(githuba);
-  const github = new Image();
-  github.src = gitIcon;
-  githuba.appendChild(github);
+const githuba = document.createElement('a');
+githuba.href = 'https://github.com/lisa-go';
+githuba.classList.add('link');
+document.querySelector('.header').appendChild(githuba);
+const github = new Image();
+github.src = gitIcon;
+githuba.appendChild(github);
